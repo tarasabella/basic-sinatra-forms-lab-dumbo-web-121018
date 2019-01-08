0c23d239f1ecc7742d530060a1a2ef9e07832fa0
Tara Sabella <tarasabella@gmail.com>
2019-01-08 16:13:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,27 @@
 require 'sinatra/base'
+require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/food_form' do
+    erb :food_form
+  end
+
+
+  post '/food' do
+    
+  end 
+ 
+  post '/food' do
+    params.to_s
+  end
+  
+  post '/food' do
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
+end
+
+
   
    get '/new_team' do
     erb :index
