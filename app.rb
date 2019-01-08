6685af36426a59e211d2f37c39054d57f4e7c4cf
Tara Sabella <tarasabella@gmail.com>
2019-01-08 15:50:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  
+   get '/' do
+    erb :index
+  end
+
+  get '/:newteam' do
+    erb params[:new_team].to_sym
+  end
+
+  post '/:team' do
+    erb :team
+  end
 
 
-end
+
+end 
